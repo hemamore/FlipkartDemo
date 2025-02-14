381dd4d5b43899758429bff5054e7af5f974121f
Amrapalisu <amrapali.suryawanshi97@gmail.com>
2025-02-14 16:49:14 +0530

    Adding choco file

---
+++ src/test/java/com/test/Choco.java
@@ -0,0 +1,13 @@
+package com.test;
+
+import org.testng.annotations.Test;
+
+public class Choco {
+	@Test
+	public void verifyUserName() {
+   
+		System.out.println("My first Method");
+		System.out.println("My method");
+	}
+
+}
